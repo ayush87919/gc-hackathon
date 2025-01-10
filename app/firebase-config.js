fb8c08909d8e9fac18676f9215a87415279110b3
Ayush Tyagi <2023csb1108@iitrpr.ac.in>
2025-01-11 00:21:50 +0530

    first push

---
+++ app/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByIKuQIakuVrxk6pKrofTOhdtboUzSayg",
+  authDomain: "publicentrydevice.firebaseapp.com",
+  projectId: "publicentrydevice",
+  storageBucket: "publicentrydevice.firebasestorage.app",
+  messagingSenderId: "487711826637",
+  appId: "1:487711826637:web:e405a74b3b0311f0cbff84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
